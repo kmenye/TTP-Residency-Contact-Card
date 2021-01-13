da2ba93c964fff5a6c3ebd8d98b88cedd7178148
Keith Menye <k.menye@gmail.com>
2021-01-12 20:24:26 -0500

    Code for COntact Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import {ContactCard} from './components/contactCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className = 'toRender'>
+        <ContactCard lastName = 'Edwards' firstName = 'Travyse'/> <br/>
+        <ContactCard lastName = 'Borhara' firstName = 'Deepak'/> <br/>
+        <ContactCard lastName = 'Lapid' firstName = 'AJ'/>
+      </div>
+    );
+  }
 }
 
 export default App;
